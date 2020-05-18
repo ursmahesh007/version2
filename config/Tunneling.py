a1eedb4d4e9c2c9cd3df0c909ea4a56139a8c619
Mahesh S R <55892813+ursmahesh007@users.noreply.github.com>
2020-05-18 01:39:37 -0400

    Update Tunneling.py

--- config/Tunneling.py
+++ config/Tunneling.py
@@ -1,6 +1,6 @@
 from sshtunnel import SSHTunnelForwarder
 tunnel = SSHTunnelForwarder(
-    '13.82.1.133',
+    '13.68.203.76',
     ssh_username="testuser",
     ssh_password="Windows@1234",
     remote_bind_address=('127.0.0.1', 27017),
